4754d5dfc86d78e912b07428432a374aeedd43fb
pknut <pknut@LAPTOP02>
2017-07-16 19:06:43 +0100

    Add project files.

---
+++ Arduino01/Arduino01.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino01.ino
+ Created:	7/16/2017 6:53:52 PM
+ Author:	pknut
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ Microchip.M24LC512/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microchip.M24LC512
+{
+    public class Class1
+    {
+    }
+}
